02c5687fb7c35fb7cedd6e2bd98693f662a335df
Savinu Hasalanka <savinu.hsa@gmail.com>
2023-01-05 08:47:05 +0530

    add dynamic urls and pages for each article

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -46,6 +47,22 @@ app.post("/compose", (req, res) => {
 });
 
 
+app.get("/posts/:requestedTitle", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.requestedTitle);
+
+  const item = posts.find((item) => {
+    const postTitle = item.title.toLowerCase();
+    if (postTitle === requestedTitle) {return item;}
+  });
+
+  if (item) {
+    res.render("post", {title: item.title, content: item.content});
+  } else {
+    console.log("match not found");
+  }
+
+  // item == undefined ? console.log("match not found") : console.log("Match found");
+});
 
 
 
